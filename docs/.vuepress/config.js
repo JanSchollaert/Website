731277860bee611a1cf27bcdc9dc18d42e4970a2
JanSchollaert <jbscholl@avans.nl>
2019-12-06 17:33:55 +0100

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+title: 'Jan Schollaert Website',
+description: 'Website gemaakt door Jan Schollaert',
+theme: 'yuu',
+base: '/',
+repo: 'JanSchollaert/Website',
+docsDir: 'docs',
+themeConfig: {
+  yuu: {
+    colorThemes: ['blue', 'red', 'purple'],
+  },
+  nav: [
+    { text: 'Home', link: '/' }
+  ],
+  sidebar: [
+      {
+        title: 'Home',
+        path: '/',
+        collapsable: false,
+      },
+      {
+        title: 'Voorbeeld markdown',
+        children: [
+          '/more-text'
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /default.png
+heroText: Jan Schollaert
+tagline: Software engineer
+actionText: Get Started →
+actionLink: test
+features:
+- title: About me
+  details: Student at Avans.
+- title: Curriclum Vitae
+  details: Very cool cv.
+- title: Portfolio
+  details: Cool things I made.
+footer: Jan Schollaert | 2019
+---
