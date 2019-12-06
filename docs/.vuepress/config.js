4c9e2af1cb43ed08b96c74d77f464ea40df65347
JanSchollaert <jbscholl@avans.nl>
2019-12-06 18:14:45 +0100

    puntenlol

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,41 +1,41 @@
 module.exports = {
-    title: 'Website',
-    theme: 'yuu',
-    base: '/Website/',
-    repo: 'JanSchollaert/Website',
-    docsDir: 'docs',
-    themeConfig: {
-      yuu: {
-        colorThemes: ['blue', 'red', 'purple']
+  title: 'Website',
+  theme: 'yuu',
+  base: '/Website/',
+  repo: 'JanSchollaert/Website',
+  docsDir: 'docs',
+  themeConfig: {
+    yuu: {
+      colorThemes: ['blue', 'red', 'purple'],
+    },
+    editLinks: true,
+    sidebarDepth: 4,
+    lastUpdated: true,
+    nav: [
+      {
+        text: 'Home',
+        link: '/',
       },
-      editLinks: true,
-      sidebarDepth: 4,
-      lastUpdated: true,
-      nav: [
+    ],
+    sidebar: {
+      '/': [
         {
-          text: 'Home',
-          link: '/'
-        }
+          title: 'Home',
+          collapsable: false,
+        },
+        {
+          title: 'About me',
+          children: [
+            'curriculum-vitae',
+          ],
+        },
+        {
+          title: 'Portfolio',
+          children: [
+            'beast-arena',
+          ],
+        },
       ],
-      sidebar: {
-        '/': [
-          {
-            title: 'Home',
-            collapsable: false
-          },
-          {
-            title: 'About me',
-            children: [
-              'curriculum-vitae'
-            ]
-          },
-          {
-            title: 'Portfolio',
-            children: [
-              'beast-arena'
-            ]
-          }
-        ]
-      }
-    }
-  }
+    },
+  },
+}
